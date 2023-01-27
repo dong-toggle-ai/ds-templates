90663c59b621014dd16928931c31b200736df6cd
Dong Wang <dong@toggle.ai>
2023-01-27 16:38:29 +0000

    Add hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+
+def main(args):
+    os.system('git init')
+
+    # Add hello-world as submodule
+    os.system('git submodule add https://github.com/dong-toggle-ai/hello-world')
+    os.chdir("hello-world")
+    os.system('git checkout main && echo main')
+    os.chdir("..")
+
+    # The checkout might have left the hello-world dirty
+    os.system("git add hello-world")
+    os.system('git commit -m "Added hello-world submodule"')
+
+
+    # Add ds-abc as submodule
+    os.system('git submodule add https://github.com/dong-toggle-ai/ds-abc')
+    os.chdir("ds-abc")
+    os.system('git checkout main && echo main')
+    os.chdir("..")
+
+    # The checkout might have left the ds-abc dirty
+    os.system("git add ds-abc")
+    os.system('git commit -m "Added ds-abc submodule"')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    args = parser.parse_args()
+
+    main(args)
