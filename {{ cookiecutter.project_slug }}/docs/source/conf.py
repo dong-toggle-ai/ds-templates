1f354c525ca55bbdd31b62adaf077eedf5bdf1d5
Dong Wang <dong@toggle.ai>
2023-02-17 14:07:19 +0000

    Update Sphinx

--- {{ cookiecutter.project_slug }}/docs/source/conf.py
+++ {{ cookiecutter.project_slug }}/docs/source/conf.py
@@ -2,6 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -27,3 +29,5 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath('../..'))
